ff5e79663475eac69c378205eb0498f5b755af1f
Tamar Nakashidze <t.nakashidze.1@gmail.com>
2024-03-18 15:36:19 +0400

    init

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+ firebase:{
+ apiKey: "AIzaSyDfhQi-4IZVsu3DsQf1MsDcVhPy4_r-JvI",
+ authDomain: "airbnb-clone-ae7ee.firebaseapp.com",
+ projectId: "airbnb-clone-ae7ee",
+ storageBucket: "airbnb-clone-ae7ee.appspot.com",
+ messagingSenderId: "99045142914",
+ appId: "1:99045142914:web:cd7727dcffdaa8d4449f9b",
+ measurementId: "G-16BQ8N5SCG"
+}
+}
